c76767fb91a839a59d060515f8d768631ed87835
SkSkFamily <Fox191921@outlook.com>
2024-10-26 12:41:36 -0600

    Second upload
    uploaded each file

---
+++ index.js
@@ -0,0 +1,9 @@
+// const greeting = document.getElementById("greeting")
+
+// greeting.style.textAlign = "center"
+// greeting.style.marginTop = "50px"
+// greeting.style.fontSize = "25px"
+// greeting.style.color = "black";
+
+// const username = window.prompt("What's your name?" , "Shaylyn");
+// greeting.innerHTML = "Welcome to my website, " + username + "!"
